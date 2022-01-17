b54ef6613f34da67f82863c5073f4a336a565f3d
Stan Tur <sturcan2000@gmail.com>
2022-01-17 13:34:15 +0000

    updated search functionality

--- src/App.js
+++ src/App.js
@@ -1,56 +1,88 @@
 import './App.css';
-import { useState,useRef } from 'react';
+import { useState, useRef } from 'react';
 import Axios from 'axios';
 import Recipe from './Recipe';
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import { InsertEmoticon, SearchOutlined } from "@material-ui/icons";
-import {InputBase } from "@material-ui/core"
+import { Button, Checkbox, FormControlLabel, IconButton } from '@material-ui/core';
+import { SearchOutlined, FilterList as Filter } from "@material-ui/icons";
+import { InputBase, Tooltip } from "@material-ui/core"
+import { TextField } from '@material-ui/core';
+import { Grid } from "@material-ui/core";
+import { orange } from '@material-ui/core/colors';
+import axios from 'axios';
 
-const useStyles = makeStyles((theme) => ({
-  cardGrid: {
-      flexGrow: 1,
-      paddingTop: 16,
-  },
-}));
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      color: "white",
+      border: "2px solid #dacd0a",
+      margin: 7,
+      "&.Mui-focused": {
+        border: "none",
+        "& .MuiOutlinedInput-notchedOutline": {
+          border: "none"
+        }
+      }
+    },
+    instructions: {
+      padding: 15, width: 260
+    }
+  })
+);
+const BlueOnGreenTooltip = withStyles({
+  tooltip: {
+    color: "white",
+    backgroundColor: "green"
+    , fontSize: "1rem",
+    padding: 10
+  }
+})(Tooltip)
 
 function App() {
-  const inputRef=useRef()
+  const inputRef = useRef()
   const [name, setName] = useState("")
+  const [isGlutenFreeChecked, setGlutenFree] = useState(false)
   // name is the state value and setName is the function used to modify it
- //I used the useState React hook to store state variables which are name,instructions,recipesList
+  //I used the useState React hook to store state variables which are name,instructions,recipesList
   const [instructions, setInstructions] = useState("");
   // const [ingredient, setIngredient] = useState[]);
   const [recipesList, setRecipesList] = useState([]);
-  const [searchedTerm,setSearchedTerm]=useState("")
-const[ingredients,setIngredients]=useState([]);
-  const addRecipe = () => {
-    if(!name || !instructions || ingredients.length==0){
+  const [searchedTerm, setSearchedTerm] = useState("")
+  const [ingredients, setIngredients] = useState([]);
+  const hideRecipes = function () {
+    setRecipesList([])
+  }
+  const addRecipe = function () {
+    if (!name || !instructions || ingredients.length == 0) {
       alert("Please input all the required fields")
       return;
     }
+    setInstructions("")
+    setName("")
+    setIngredients([])
     Axios.post("http://localhost:3001/create", {
       name: name,
       instructions: instructions,
-      ingredients
-      
+      ingredients,
+      isGlutenFree: isGlutenFreeChecked
+
     }).then((res) => {
       //Response from the node.js/express backend
-  
-      setRecipesList(res.data)
+
+      setRecipesList(res.data.filter(function (item) { return item.name === name && item.instructions === instructions }))
       console.log("success");
     });
   };
-  const addIngredients=()=>{
-    setIngredients([...ingredients,inputRef.current.value])  
-    inputRef.current.value=""
+  const addIngredients = function () {
+    setIngredients([...ingredients, inputRef.current.value])
+    inputRef.current.value = ""
   }
-//this functions makes a request to our backend which is running on port 3001 and it gets all the recipes in the response
-// and upon getting all the recipes it sets the recipe list state variable to the response using the functions provided by the useState hook
-//
-  const getRecipes = () => {
+  //this functions makes a request to our backend which is running on port 3001 and it gets all the recipes in the response
+  // and upon getting all the recipes it sets the recipe list state variable to the response using the functions provided by the useState hook
+  //
+  const getRecipes = function () {
     Axios.get("http://localhost:3001/recipes").then((response) => {
       console.log(response);
       setRecipesList(response.data);
@@ -59,97 +91,152 @@ const[ingredients,setIngredients]=useState([]);
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
- const handleSearch=()=>{
-   const data=recipesList.map((item)=>{
-   
-      if(item.ingredients)  {
-        console.log(searchedTerm,item.ingredients,"Search term adn item ingredients")
-       if(item.ingredients.includes(capitalizeFirstLetter(searchedTerm)) || item.ingredients.includes(searchedTerm) ){
-         console.log(item)
-         return item
-       }
-        else if(item.ingredients.includes(searchedTerm.toLowerCase())  || item.ingredients.includes(searchedTerm) ){
+  const handleFilter = function () {
+    const data = recipesList.filter(function (item) {
+      return item.isGlutenFree
+    })
+    setRecipesList(data)
+  }
+  const handleSearch = async function () {
+    const res = await axios.get("http://localhost:3001/recipes")
+    const data = res.data.map((item) => {
+
+      if (item.ingredients) {
+        console.log(searchedTerm, item.ingredients, "Search term and item ingredients")
+        if (item.ingredients.includes(capitalizeFirstLetter(searchedTerm)) || item.ingredients.includes(searchedTerm)) {
+          console.log(item)
           return item
         }
-      
-      } 
-    }).filter(item=>item && item)
-  
-  setRecipesList(data)
-  console.log(data)
-  
- }
+        else if (item.ingredients.includes(searchedTerm.toLowerCase()) || item.ingredients.includes(searchedTerm)) {
+          return item
+        }
+
+      }
+    }).filter(item => item && item)
+
+
+    const nameResult = res.data.filter((item) => {
+      return item.name.toLowerCase().includes(searchedTerm.toLowerCase())
+    })
+    if (data.length === 0 && nameResult.length > 0) {
+      setSearchedTerm("")
+      setRecipesList(nameResult)
+      return
+    }
+    if (data.length === 0) {
+      setSearchedTerm("")
+      alert("This Item Does Not exist!.")
+    }
+    setSearchedTerm("")
+    setRecipesList(data)
+    console.log(data)
+
+  }
   const classes = useStyles();
+
   //a Material-UI hook to get access to the classes in the makeStyles function on the top
 
   return (
-    <div className="App">
-      <div className="RecipeForm">
-  
-     <div style={{display:"flex",justifyContent:"space-between",position:"absolute",top:100,right:100,width:350,backgroundColor:"white",padding:20}}> 
-      <InputBase
-      value={searchedTerm}
-      onChange={e=>setSearchedTerm(e.target.value)}
-        placeholder="find recipes by ingredients"
-        inputProps={{ 'aria-label': 'search recipes' }}
-      />
-        <SearchOutlined   onClick={handleSearch} />
-    </div>
-      <label>Recipe Name:</label>
-      <input
-        type="text"
-        onChange={(event) => {
-          //function setting name to event.target.value
-          setName(event.target.value);
-        }}
-      />
-      <label>Recipe Instruction:</label>
-      <textarea
-        type="text"
-        onChange={(event) => {
-          setInstructions(event.target.value);
-        }}
-      />
-     
-  <label>Ingredients</label>
-        <input type="text" ref={inputRef}   onKeyPress={e=>{
-        if(e.charCode===13){
-          addIngredients()
-        };
-        return
-      }}  />
-      <button onClick={addIngredients}>Add Ingredient</button>
+    <div className="App" style={{ display: "flex" }}>
+      <div className="RecipeForm" style={{ marginTop: 150 }}>
 
-      <div style={{backgroundColor:"white",minHeight:50,minWidth:292,marginTop:10,padding:12}}>
-     { ingredients.length>=1 &&  <h3>Ingredients</h3>}
-       
-        <ul>
-      {ingredients.map(ingredient=><li key={ingredient}>{ingredient} </li>)}
-      </ul>
+        <div>
+          <div style={{ display: "flex", justifyContent: "space-between", position: "absolute", top: 35, right: 50, borderRadius: 8, width: 350, backgroundColor: "white", padding: 20 }}>
+            <InputBase
+              value={searchedTerm}
+              onChange={e => setSearchedTerm(e.target.value)}
+              placeholder="search by name/ingredients"
+              inputProps={{ 'aria-label': 'search recipes' }}
+            />
+            <IconButton style={{ width: 30, height: 30 }}>
+              <SearchOutlined onClick={handleSearch} />
+            </IconButton>
+
+            <BlueOnGreenTooltip title="Filter for Gluten Free" placement="top">
+              <IconButton style={{ height: 20, width: 20 }} onClick={handleFilter}>
+                <Filter />
+              </IconButton>
+
+            </BlueOnGreenTooltip>
+
+          </div>
+
+        </div>
+
+        <label style={{ fontFamily: '"Lobster",cursive', fontSize: 30 }}>Recipe Name:</label>
+        <TextField
+          style={{ padding: 5, margin: 3 }}
+          type="text"
+          value={name}
+          InputProps={{ className: classes.root }}
+          className={classes.textfield}
+          onChange={(event) => {
+            //function setting name to event.target.value
+            setName(event.target.value);
+          }}
+        />
+        <label style={{ fontFamily: '"Lobster",cursive', fontSize: 30 }}>Recipe Instructions:</label>
+        <TextField
+
+          type="text"
+          InputProps={{ className: `${classes.root} ${classes.instructions}` }}
+          className={classes.textfield}
+          value={instructions}
+          onChange={(event) => {
+            setInstructions(event.target.value);
+          }}
+        />
+
+        <label style={{ fontFamily: '"Lobster",cursive', fontSize: 30 }}>Ingredients:</label>
+        <TextField
+          inputRef={inputRef}
+          style={{ marginBottom: 7 }}
+          type="text"
+          InputProps={{ className: `${classes.root} ${classes.instructions}` }}
+          className={classes.textfield}
+          onKeyPress={e => {
+            if (e.charCode === 13) {
+              addIngredients()
+            };
+            return
+          }}
+        />
+
+
+        <Button onClick={addIngredients} style={{ backgroundColor: "#2196f3", borderRadius: 5, color: "white" }} color="primary" variant='contained' size="large">Add Ingredient </Button>
+        {ingredients.length >= 1 && <div style={{ backgroundColor: "white", minHeight: 50, minWidth: 292, marginTop: 10, padding: 12 }}>
+          <h3>Ingredients</h3>
+
+          <ul>
+            {ingredients.map(ingredient => <li key={ingredient}>{ingredient} </li>)}
+          </ul>
+        </div>}
+        <FormControlLabel control={<Checkbox checked={isGlutenFreeChecked} onChange={e => setGlutenFree(e.target.checked)} />} label="Is Gluten Free?" />
+        <Button onClick={addRecipe} style={{ backgroundColor: "#2196f3", borderRadius: 5, marginBottom: 10, color: "white" }} color="primary" variant='contained' size="large">Add Recipe </Button>
+        <Button onClick={getRecipes} style={{ backgroundColor: "#33eb91", height: 40, borderRadius: 5, marginBottom: 10, color: "white" }} color="primary" variant='contained' size="large">Show All Recipes </Button>
+        <Button onClick={hideRecipes} style={{ backgroundColor: "#994477", height: 40, borderRadius: 5, color: "white" }} color="primary" variant='contained' size="large">Hide All Recipes </Button>
       </div>
-      <button onClick={addRecipe}>Add Recipe</button>
-      <button onClick={getRecipes}>Show All Recipes</button>
-      </div>
-      <Container className={classes.cardGrid} maxWidth="xl">
-      <Grid
+      <Container maxWidth="xl">
+        <Grid
           container
           direction="row"
           justifyContent="space-evenly"
           alignItems="stretch"
           spacing={2}
-      >
-        {/* mapping over the array of recipeLists and returning the Recipe component for each item inside the recipeLists array*/}
-        {recipesList.map(recipe => (
-        <Recipe
-        recipe={recipe}
-        setRecipesList={setRecipesList}
-          key = {`${recipe.name} ${Math.random()}`}
-          name = {recipe.name}
-          instructions = {recipe.instructions}
-          // ingredients = {recipe.ingredients} 
-        />
-        ))}
-      </Grid>
+        >
+          {/* mapping over the array of recipeLists and returning the Recipe component for each item inside the recipeLists array*/}
+          {recipesList.map(recipe => (
+            <Recipe
+              recipe={recipe}
+              recipesList={recipesList}
+              setRecipesList={setRecipesList}
+              key={`${recipe.name} ${Math.random()}`}
+              name={recipe.name}
+              instructions={recipe.instructions}
+            // ingredients = {recipe.ingredients} 
+            />
+          ))}
+        </Grid>
       </Container>
     </div>
   );
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -13,104 +13,113 @@ import axios from "axios"
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import  { DeleteOutlined,EditOutlined} from "@material-ui/icons"
+import { DeleteOutlined, EditOutlined } from "@material-ui/icons"
 const useStyles = makeStyles((theme) => ({
-    card: {
-        flex: 1,
-        height: '100%',
-        marginTop: '10px',
-    },
-    cardActionArea: {
-        flex: 1,
-        height: '100%',
-        backgroundColor: 'green',
-    },
-    cardMedia: {
-        height: '0',
-        paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-        flex: 1,
-        // backgroundColor: blue[600],
-    },
-    cardContentTitle: {
-        flex: 1,
-        // backgroundColor: blue[400],
-    },
-    root: {
-      position:"relative",
-      maxWidth: 345,
-    },
-    media: {
-      height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-      transform: 'rotate(0deg)',
-      marginLeft: 'auto',
-      transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-      }),
-    },
-    expandOpen: {
-      transform: 'rotate(180deg)',
-    },
-    avatar: {
-      backgroundColor: red[500],
-    },
-  }));
-  
-  
-const Recipe = ({name, instructions,recipe,setRecipesList}) => {
-  const [editState,setEditState]=useState(false)
-  const [editName,setEditName]=useState("")
-  const [editInstructions,setEditInstructions]=useState("")
-    const classes = useStyles();
-    const [expanded, setExpanded] = useState(false);
-  
-    const handleExpandClick = () => {
-      setExpanded(!expanded);
-    };
-    const handleDelete=(id)=>{
-      
-  axios.delete(`http://localhost:3001/delete/${id}`).then(d=>{
-    setRecipesList(d.data)
-  });
-    
-    }
-    const handleEditSave =async(id)=>{
-    const response=await axios.post(`http://localhost:3001/edit/${id}`,{name:editName,instructions:editInstructions})
-    setRecipesList(response.data);
-    setExpanded(false)
-   console.log(response)
-   setTimeout(()=>{
-   alert("saved")
-   },1000)
+  card: {
+    flex: 1,
+    height: '100%',
+    marginTop: '10px',
+  },
+  cardActionArea: {
+    flex: 1,
+    height: '100%',
+    backgroundColor: 'green',
+  },
+  cardMedia: {
+    height: '0',
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flex: 1,
+    // backgroundColor: blue[600],
+  },
+  cardContentTitle: {
+    flex: 1,
+    // backgroundColor: blue[400],
+  },
+  root: {
+    marginBottom: "15px",
+    position: "relative",
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
-    }
-    // console.log(recipe.ingredients)
-    const handleEditClick=()=>{
+
+const Recipe = ({ name, instructions, recipe, setRecipesList, recipesList }) => {
+  const [editState, setEditState] = useState(false)
+  const [editName, setEditName] = useState(recipe.name)
+  const [editInstructions, setEditInstructions] = useState(recipe.instructions)
+  const [ingredient, setIngredient] = useState("")
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const handleDelete = (id) => {
+
+    axios.delete(`http://localhost:3001/delete/${id}`).then(d => {
+      setRecipesList(recipesList.filter(function (item) {
+        return item.id !== id
+      }))
+    });
+
+  }
+  const handleEditSave = async (id) => {
+    const ingredients = JSON.parse(recipe.ingredients)
+    const newIngredients = [...ingredients, ingredient]
+
+    const response = await axios.post(`http://localhost:3001/edit/${id}`, { name: editName, instructions: editInstructions, ingredients: newIngredients })
+    setRecipesList(response.data.filter(function (item) {
+      return item.name === editName
+    }));
+    setExpanded(false)
+    console.log(response)
+    setTimeout(() => {
+      alert("saved")
+    }, 1000)
+
+  }
+  // console.log(recipe.ingredients)
+  const handleEditClick = () => {
     console.log(recipe.ingredients)
     setEditState(true)
-    }
-    return (
-        <Card className={classes.root}>
-        <CardHeader
-          title={name}
-        />
-        
-      
-        <CardMedia
-          className={classes.media}
-          image="/images/img1.jpeg"
-          title={name}
-        />
-        <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-          </Typography>
-        </CardContent>
-        <CardActions>
-       
+  }
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        title={name}
+      />
+
+
+      <CardMedia
+        className={classes.media}
+        image="/images/img1.jpeg"
+        title={name}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+        </Typography>
+      </CardContent>
+      <CardActions>
+
         <IconButton
           className={clsx(classes.expand, {
             [classes.expandOpen]: expanded,
@@ -122,40 +131,50 @@ const Recipe = ({name, instructions,recipe,setRecipesList}) => {
           <ExpandMoreIcon />
         </IconButton>
       </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <IconButton onClick={handleEditClick} style={{position:"absolute",top:0,right:50,width:"20%"}}>
-              <EditOutlined/>
-            </IconButton>
-          <IconButton onClick={()=>handleDelete(recipe.id)} style={{position:"absolute",top:0,right:0,width:"20%"}}>
-          <DeleteOutlined />
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+
+        <CardContent>
+          <IconButton onClick={handleEditClick} style={{ position: "absolute", top: 0, right: 50, width: "20%" }}>
+            <EditOutlined />
           </IconButton>
+          <IconButton onClick={() => handleDelete(recipe.id)} style={{ position: "absolute", top: 0, right: 0, width: "20%" }}>
+            <DeleteOutlined />
+          </IconButton>
+          <div style={{
+            backgroundColor: recipe.isGlutenFree ? "#22bb33" : "#f0ad4e",
+            color: "white", padding: 20
+          }}>
+
+
+            {recipe.isGlutenFree ? <span>This is gluten-free</span> : <span>This is not gluten-free </span>}
+
+          </div>
           {editState && (
             <>
-            <TextField value={editName} onChange={(event)=>setEditName(event.target.value)} label="name"/>
-            <TextField value={editInstructions} onChange={(event)=>setEditInstructions(event.target.value)} label="instructions"/>
-            <TextField label="Ingredients"/>
-            <div>
-              <Button onClick={()=>setEditState(false)}>Close</Button>
-              <Button onClick={()=>handleEditSave(recipe.id)} disabled={!editName && !editInstructions}> Save</Button>
-           
-            </div>
+              <TextField value={editName} onChange={(event) => setEditName(event.target.value)} label="name" />
+              <TextField value={editInstructions} onChange={(event) => setEditInstructions(event.target.value)} label="instructions" />
+              <TextField label="Add Ingredients" value={ingredient} onChange={(e) => setIngredient(e.target.value)} />
+              <div>
+                <Button onClick={() => setEditState(false)}>Close</Button>
+                <Button onClick={() => handleEditSave(recipe.id)} disabled={!editName && !editInstructions}> Save</Button>
+
+              </div>
             </>
           )}
           <Typography paragraph>Ingredients:</Typography>
-            <ul>
-              {/* {recipe.ingredients && recipe.ingredients.map(ing=><li key={ing}>{ing} </li>)} */}
-              {recipe.ingredients && JSON.parse(recipe.ingredients).map(item=><li>{item}</li>)}
-            </ul>
-            <Typography paragraph>Instructions to prepare:</Typography>
-            <Typography paragraph>
+          <ul>
+            {/* {recipe.ingredients && recipe.ingredients.map(ing=><li key={ing}>{ing} </li>)} */}
+            {recipe.ingredients && JSON.parse(recipe.ingredients).map(item => <li key={item} >{item}</li>)}
+          </ul>
+          <Typography paragraph>Instructions to prepare:</Typography>
+          <Typography paragraph>
             {instructions}
-            </Typography>
+          </Typography>
 
-          </CardContent>
-        </Collapse>
-      </Card>
-    );
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 };
 
 export default Recipe;
